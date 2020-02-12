0c7863565acc9ffaa8d0d3aafd9b0a4c265a003a
Rafi Ullah <rraffiu@gmail.com>
2020-02-11 18:20:55 -0800

    File reading with np loadtxt.

--- General/pdfplot.py
+++ General/pdfplot.py
@@ -10,35 +10,20 @@ rcParams.update({'figure.autolayout': True})
 
 # Data for plotting
 fname = sys.argv[1]
-T = []
-u1 = []
-u2 = []
-with open(fname,'r') as f:
-    line = f.readline()
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        x,y,z = [float(s) for s in line.split()]
-        T.append(x)
-        u1.append(y)
-        u2.append(z)
-
-Ta = np.asarray(T)
-ucm = np.asarray(u1)
-uncm = np.asarray(u2)
+T,u_uncor,u_cor = np.loadtxt(fname,dtype='float',comments='#',unpack=True)
 
 fig, ax = plt.subplots()
-ln1, = ax.plot(Ta,ucm,'-or')
-ln2, = ax.plot(Ta,uncm,'-db')
+ln1, = ax.plot(T,u_uncor,'-or')
+ln2, = ax.plot(T,u_cor,'-db')
 ln1.set_label(r'\bf{with drift}')
 ln2.set_label(r'\bf{corrected}')
 ax.legend(fontsize=12)
-plt.xticks((300,600,900,1200,1500), size=16)
+#plt.xticks((300,600,900,1200,1500,1800,2100,2400,2700,3000,3300), size=16)
+plt.xticks(size=16)
 plt.yticks(size=16)
 
 ax.set_xlabel(r'\bf{T (K)}',fontsize=16)
 ax.set_ylabel(r'\bf{MSD} (\AA$^2$)',fontsize=16)
 
-fig.savefig("test.pdf")
+fig.savefig("msd-P44GPa.pdf")
 plt.show(block=False)
