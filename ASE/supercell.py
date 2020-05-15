ad707604c18a57207b4f8dc980bb4ad31bf03b7d
Rafi Ullah <48779146+rraffiu@users.noreply.github.com>
2020-05-14 17:26:19 -0700

    Update supercell.py

--- ASE/supercell.py
+++ ASE/supercell.py
@@ -1,11 +1,12 @@
 from ase.build import bulk
+from ase import io
 from ase.lattice.cubic import BodyCenteredCubic
 from ase.units import Bohr
 
 # To create a supcell of 3x3x3 conventional cubic cells
 # a1 = bulk('W', 'bcc', a=3.165,cubic=True) *  (3, 3, 3)
 # To save the coordinates in xyz format
-# ase.io.write('W108.xyz',a1,'xyz')
+# io.write('W108.xyz',a1,'xyz')
 
 # To create a supercell with surfaces [110][-110][001] of 4x2x3 cells.
 # The atomic coordinates in this case are in atomic units. 
